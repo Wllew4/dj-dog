85446f7014f16165d45f8a1af3bc3b24e452338d
Foshka <foshka@uwu.pet>
2021-09-17 18:27:08 +0200

    replaced ytdl with youtube-dl+ffmpeg

--- src/bot/AudioManager.ts
+++ src/bot/AudioManager.ts
@@ -1,9 +1,9 @@
 import { waitForMs } from "../util/util";
 import { createReadStream } from 'fs';
-import { AudioPlayer, AudioPlayerError, AudioResource, createAudioPlayer, createAudioResource, demuxProbe, NoSubscriberBehavior, StreamType, VoiceConnection } from "@discordjs/voice";
+import { AudioPlayer, AudioResource, createAudioPlayer, createAudioResource, demuxProbe, NoSubscriberBehavior, StreamType } from "@discordjs/voice";
 import { TrackInfo } from "./Track";
-import ytdl, { downloadOptions } from 'ytdl-core';
-
+import youtubedl, { raw as youtubedlraw } from 'youtube-dl-exec';
+import { Converter } from "ffmpeg-stream";
 
 export class AudioManager
 {
@@ -32,30 +32,33 @@ export class AudioManager
   {
     const resource = await this.probeAndCreateResource(trackInfo.path);
     this.audioPlayer.play(resource);
-    
+
     await waitForMs(trackInfo.duration * 1000);
   }
 
-  public async stream(url: string, starttime=0) {
+  public async stream(url: string) {
     try{
-      let dlOpts: downloadOptions = {
-        filter: 'audioonly',
-        highWaterMark: 16777216,
-        begin: starttime + 'ms'
-      };
-      const basicInfo = await ytdl.getBasicInfo(url);
-      const resource = createAudioResource(ytdl(url, dlOpts), { inputType: StreamType.WebmOpus });
+      // set up ffmpeg
+      const converter = new Converter();
+      const input = converter.createInputStream({});
+      const output = converter.createOutputStream({f: "opus", b: '192000'});
+
+      // download video & convert
+      youtubedlraw(url, {f:'bestaudio', o:'-'}).stdout?.pipe(input);
+      converter.run();
+
+      // Discord stuff
+      const resource = createAudioResource(output, { inputType: StreamType.OggOpus });
       this.audioPlayer.play(resource);
-      this.audioPlayer.on('error', err => {
-        console.error(err);
-        if (err instanceof AudioPlayerError) this.stream(url, starttime + err.resource.playbackDuration);
-      });
-      const msToWait = parseInt(basicInfo.videoDetails.lengthSeconds) * 1000 - starttime;
-      await waitForMs(msToWait);
+      this.audioPlayer.on('error', console.error );
+
+      // resolve promise when song is done
+      const { duration } = await youtubedl(url, { dumpSingleJson:true });
+      await waitForMs(duration * 1000);
+      converter.kill();
     }
     catch(err){
       console.error(err);
-      if (err instanceof AudioPlayerError) this.stream(url, starttime + err.resource.playbackDuration);
     }
   }
 
