ca5b106cece1ebbedf2b3c09f99517fe8470ef1e
Foshka <foshka@uwu.pet>
2021-09-18 02:46:28 +0200

    fixed search

--- src/bot/Search.ts
+++ src/bot/Search.ts
@@ -1,24 +1,34 @@
+// activate api & get key from https://console.cloud.google.com/apis/library/youtube.googleapis.com
 import { youtube_api_key } from '../confidential.json';
-import nodeFetch from "node-fetch";
+import fetch from 'cross-fetch';
 
 class Search{
   static async search (query: string): Promise<string> {
-    const body = {
-      key: youtube_api_key,
-      q: query,
-      maxResults: 1,
-      type: 'video',
-      videoCategoryId: 10
-    };
-    const res = await nodeFetch('https://www.googleapis.com/youtube/v3/search', {
-      body: JSON.stringify(body)
-    });
+    const res = await fetch(`//www.googleapis.com/youtube/v3/search?key=${youtube_api_key}&q=${query}&maxResults=1&type=video&videoCategoryId=10`, { method: 'GET' });
+
+    if (!res.ok) {
+      console.error(res);
+      throw new Error("Bad response from server");
+    }
+
+    const resJson = await res.json();
+    console.log(resJson);
+
     // @ts-ignore
-    const searchResult = res.body.items[0];
+    const searchResult = resJson.items[0];
     if (!searchResult) {
       throw Error(`No video found for query: ${query}`);
     }
-    return `https://www.youtube.com/watch&v=${searchResult.id.videoId}`;
+    return `https://www.youtube.com/watch?v=${searchResult.id.videoId}`;
+  }
+
+  static isValidUrl (url: string): boolean {
+    return (
+      url.startsWith('https://') ||
+      url.startsWith('http://') ||
+      url.startsWith('www.youtube.com/') ||
+      url.startsWith('youtube.be/')
+    );
   }
 }
 
