452fe5d9c4310d1604d78b3a613ed7766d2ec929
Foshka <foshka@uwu.pet>
2021-09-20 04:40:58 +0200

    use pm2 to restart

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 cd ~/app
 npm ci
 ./node_modules/.bin/tsc -p tsconfig.json
-tmux kill-session -t djdog || true
-tmux new-session -d -s djdog || true
-tmux send-keys 'node build/index.js' C-m || true
-tmux detach -s djdog || true
