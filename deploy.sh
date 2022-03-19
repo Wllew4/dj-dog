676f0a5c4822c0d165c51e4626487027fa549f4c
Wllew4 <willjanelle2@gmail.com>
2022-03-19 12:14:04 -0400

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 # Deploy
 # Install deps
-chmod -R +x ./scripts
+chmod -R +x /scripts
 ./scripts/install-node.sh
 ./scripts/install-python.sh
 ./scripts/install-ffmpeg.sh
+# Build bot
+source ~/.bashrc
 ./scripts/build.sh
 # Run
 node build/index.js
