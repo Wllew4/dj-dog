ba8b9b24884f04367665fba5b2a6b97be7c69f48
Wllew4 <willjanelle2@gmail.com>
2022-03-19 14:53:29 -0400

    full ffmpeg compile script

--- scripts/install-ffmpeg.sh
+++ scripts/install-ffmpeg.sh
@@ -1,132 +1,146 @@
 # Compile FFmpeg
-## 1 Assemblers
-### 1.1 Dependencies
-# sudo yum install autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make pkgconfig zlib-devel
-sudo apt install \
+
+# Compilation dependencies
+sudo apt-get update -qq && sudo apt-get -y install \
 	autoconf \
 	automake \
-	bzip2 \
-	bzip2-devel \
+	build-essential \
 	cmake \
-	freetype-devel \
-	gcc \
-	gcc-c++ \
-	git \
+	git-core \
+	libass-dev \
+	libfreetype6-dev \
+	libgnutls28-dev \
+	libmp3lame-dev \
+	libsdl2-dev \
 	libtool \
-	make \
-	pkgconfig \
-	zlib-devel
-# sudo apt -y install \
-# 	autoconf \
-# 	automake \
-# 	build-essential \
-# 	cmake \
-# 	git-core \
-# 	libass-dev \
-# 	libfreetype6-dev \
-# 	libgnutls28-dev \
-# 	libmp3lame-dev \
-# 	libsdl2-dev \
-# 	libtool \
-# 	libva-dev \
-# 	libvdpau-dev \
-# 	libvorbis-dev \
-# 	libxcb1-dev \
-# 	libxcb-shm0-dev \
-# 	libxcb-xfixes0-dev \
-# 	meson \
-# 	ninja-build \
-# 	pkg-config \
-# 	texinfo \
-# 	wget \
-# 	yasm \
-# 	zlib1g-dev
-mkdir ~/ffmpeg_sources
-### 2.1 NASM
-cd ~/ffmpeg_sources
-curl -O -L https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2
-tar xjvf nasm-2.15.05.tar.bz2
-cd nasm-2.15.05
-./autogen.sh
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
-make
+	libva-dev \
+	libvdpau-dev \
+	libvorbis-dev \
+	libxcb1-dev \
+	libxcb-shm0-dev \
+	libxcb-xfixes0-dev \
+	meson \
+	ninja-build \
+	pkg-config \
+	texinfo \
+	wget \
+	yasm \
+	zlib1g-dev
+
+sudo apt install libunistring-dev libaom-dev libdav1d-dev
+
+mkdir -p ~/ffmpeg_sources ~/bin
+
+# Compilation & Installation
+# NASM
+cd ~/ffmpeg_sources && \
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2 && \
+tar xjvf nasm-2.15.05.tar.bz2 && \
+cd nasm-2.15.05 && \
+./autogen.sh && \
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
+make && \
 make install
-### 2.2 Yasm
-cd ~/ffmpeg_sources
-curl -O -L https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-tar xzvf yasm-1.3.0.tar.gz
-cd yasm-1.3.0
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
-make
+# libx264
+cd ~/ffmpeg_sources && \
+git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git && \
+cd x264 && \
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic && \
+PATH="$HOME/bin:$PATH" make && \
 make install
-## 3.1 libraries
-### 3.1.1 libx264
-cd ~/ffmpeg_sources
-git clone --branch stable --depth 1 https://code.videolan.org/videolan/x264.git
-cd x264
-PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
-make
+# libx265
+sudo apt-get install libnuma-dev && \
+cd ~/ffmpeg_sources && \
+wget -O x265.tar.bz2 https://bitbucket.org/multicoreware/x265_git/get/master.tar.bz2 && \
+tar xjvf x265.tar.bz2 && \
+cd multicoreware*/build/linux && \
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off ../../source && \
+PATH="$HOME/bin:$PATH" make && \
 make install
-### 3.1.2 libx265
-cd ~/ffmpeg_sources
-git clone --branch stable --depth 2 https://bitbucket.org/multicoreware/x265_git
-cd ~/ffmpeg_sources/x265_git/build/linux
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
-make
+# libvpx
+cd ~/ffmpeg_sources && \
+git -C libvpx pull 2> /dev/null || git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git && \
+cd libvpx && \
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm && \
+PATH="$HOME/bin:$PATH" make && \
 make install
-### 3.1.3 libfdk_aac
-cd ~/ffmpeg_sources
-git clone --depth 1 https://github.com/mstorsjo/fdk-aac
-cd fdk-aac
-autoreconf -fiv
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
-make
+# libfdk-aac
+cd ~/ffmpeg_sources && \
+git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac && \
+cd fdk-aac && \
+autoreconf -fiv && \
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
+make && \
 make install
-### 3.1.4 libmp3lame
-cd ~/ffmpeg_sources
-curl -O -L https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
-tar xzvf lame-3.100.tar.gz
-cd lame-3.100
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
-make
+# libopus
+cd ~/ffmpeg_sources && \
+git -C opus pull 2> /dev/null || git clone --depth 1 https://github.com/xiph/opus.git && \
+cd opus && \
+./autogen.sh && \
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
+make && \
 make install
-### 3.1.5 libopus
-cd ~/ffmpeg_sources
-curl -O -L https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
-tar xzvf opus-1.3.1.tar.gz
-cd opus-1.3.1
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
-make
+# libaom
+cd ~/ffmpeg_sources && \
+git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom && \
+mkdir -p aom_build && \
+cd aom_build && \
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_TESTS=OFF -DENABLE_NASM=on ../aom && \
+PATH="$HOME/bin:$PATH" make && \
 make install
-### 3.1.6 libvpx
-cd ~/ffmpeg_sources
-git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
-cd libvpx
-./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm
-make
+# libsvtav1
+cd ~/ffmpeg_sources && \
+git -C SVT-AV1 pull 2> /dev/null || git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git && \
+mkdir -p SVT-AV1/build && \
+cd SVT-AV1/build && \
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF .. && \
+PATH="$HOME/bin:$PATH" make && \
 make install
-## FFmpeg
-cd ~/ffmpeg_sources
-curl -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
-tar xjvf ffmpeg-snapshot.tar.bz2
-cd ffmpeg
+# libdav1d
+cd ~/ffmpeg_sources && \
+git -C dav1d pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/dav1d.git && \
+mkdir -p dav1d/build && \
+cd dav1d/build && \
+meson setup -Denable_tools=false -Denable_tests=false --default-library=static .. --prefix "$HOME/ffmpeg_build" --libdir="$HOME/ffmpeg_build/lib" && \
+ninja && \
+ninja install
+# libvmaf
+cd ~/ffmpeg_sources && \
+wget https://github.com/Netflix/vmaf/archive/v2.1.1.tar.gz && \
+tar xvf v2.1.1.tar.gz && \
+mkdir -p vmaf-2.1.1/libvmaf/build &&\
+cd vmaf-2.1.1/libvmaf/build && \
+meson setup -Denable_tests=false -Denable_docs=false --buildtype=release --default-library=static .. --prefix "$HOME/ffmpeg_build" --bindir="$HOME/bin" --libdir="$HOME/ffmpeg_build/lib" && \
+ninja && \
+ninja install
+# FFmpeg
+cd ~/ffmpeg_sources && \
+wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 && \
+tar xjvf ffmpeg-snapshot.tar.bz2 && \
+cd ffmpeg && \
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
-	--prefix="$HOME/ffmpeg_build" \
-	--pkg-config-flags="--static" \
-	--extra-cflags="-I$HOME/ffmpeg_build/include" \
-	--extra-ldflags="-L$HOME/ffmpeg_build/lib" \
-	--extra-libs=-lpthread \
-	--extra-libs=-lm \
-	--bindir="$HOME/bin" \
-	--enable-gpl \
-	--enable-libfdk_aac \
-	--enable-libfreetype \
-	--enable-libmp3lame \
-	--enable-libopus \
-	--enable-libvpx \
-	--enable-libx264 \
-	--enable-libx265 \
-	--enable-nonfree
-make
-make install
-hash -d ffmpeg
+  --prefix="$HOME/ffmpeg_build" \
+  --pkg-config-flags="--static" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" \
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --extra-libs="-lpthread -lm" \
+  --ld="g++" \
+  --bindir="$HOME/bin" \
+  --enable-gpl \
+  --enable-gnutls \
+  --enable-libaom \
+  --enable-libass \
+  --enable-libfdk-aac \
+  --enable-libfreetype \
+  --enable-libmp3lame \
+  --enable-libopus \
+  --enable-libsvtav1 \
+  --enable-libdav1d \
+  --enable-libvorbis \
+  --enable-libvpx \
+  --enable-libx264 \
+  --enable-libx265 \
+  --enable-nonfree && \
+PATH="$HOME/bin:$PATH" make && \
+make install && \
+hash -r
